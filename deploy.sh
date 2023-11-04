21d41d3615ac408b1d394504ee8ce65511d33478
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-04 21:19:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 sh <<'EOF'
 if [ "$GIT_BRANCH" == "origin/main" ]; then
-    withCredentials([usernamePassword(credentialsId: 'Docker_credential', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
         docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
         docker tag reactapp:latest reactapp:v1
         docker push faizul04/prod:latest
