8f6fc8adf7517c257766af5f557f16911de46996
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-04 21:33:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sh <<EOF
 docker build -t reactappimg .
-docker run -d --name newcont -p 80:3000 reactappimg
+docker run -d --name newcont -p 9000:8000 reactappimg
 docker-compose down || true
 docker-compose up -d
 EOF
