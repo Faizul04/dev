d84a889b998101bd81271b9b3dfc6035d64fcaa6
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-05 22:12:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ["$GIT_BRANCH" == "origin/main" ]; then
+if ["$GIT_BRANCH" = "origin/main" ]; then
         build.sh
         docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD docker.io
         docker tag reactapp:latest faizul04/prod:v1
