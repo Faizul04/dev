4bd2d63546f2eb9acaaafcadb43689c5415da3b8
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-06 14:03:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 docker build -t reactappimg:latest .
 docker tag reactappimg:latest faizul04/dev/reactapp:latest
 docker tag reactappimg:latest faizul04/prod/reactapp:latest
-docker run -d --name kiki -p 8090:8000 reactappimg
-docker-compose down || true
-docker-compose up -d
+
