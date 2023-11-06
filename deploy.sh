f8dfc6085b30fe5bf215c22c215f3ab4fc934bb6
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-06 13:14:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 if [[ -n "$GIT_BRANCH" && "$GIT_BRANCH" == "origin/main" ]]; then
     build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker tag reactapp:latest faizul04/prod:latest
+    docker tag reactappimg:latest faizul04/prod:latest
     docker push faizul04/prod:latest
     docker logout
 else
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker tag reactapp:latest faizul04/dev:latest
+    docker tag reactappimg:latest faizul04/dev:latest
     docker push faizul04/dev:latest
     docker logout
 fi
