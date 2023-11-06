9cc25351e01eb5fb8228b0cd31470a2c053fb13a
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-06 22:14:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker build -t reactappimg:latest .
+docker run -d --name faizul -p 8030:8000 reactappimg
 docker tag reactappimg:latest faizul04/dev/reactapp:latest
 docker tag reactappimg:latest faizul04/prod/reactapp:latest
 
