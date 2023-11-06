a44a83461be12abde661135ce77d65efebcc9ef0
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-06 13:20:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 docker build -t reactappimg:latest 
 docker build -t reactappimg:latest,faizul04/dev/reactapp:latest faizul04/prod
-docker run -d --name test -p 9000:8000 reactappimg
+docker run -d --name test -p 9090:8000 reactappimg
 docker-compose down || true
 docker-compose up -d
