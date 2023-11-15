398505810c2b306f51e897b3bebaf030049bbd96
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-15 12:11:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-
-sh <<EOF
-docker build -t reactappimg .
-docker run -d --name container5t -p 8090:8000 reactappimg
-docker-compose down || true
-docker-compose up -d
-EOF
-
-
-
 docker build -t capstoneimg:latest .
 docker run -d --name Trustcont -p 3000:8000 capstoneimg
 docker tag capstoneimg:latest faizul04/dev/capstoneimg:latest
