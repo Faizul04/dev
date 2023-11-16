3aa8d15cd3602a154fac5efd49f47b192f06704e
Faizul04 <144917443+Faizul04@users.noreply.github.com>
2023-11-16 11:07:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t capstoneimg:latest .
-docker run -itd -p 3000:8000 capstoneimg
+docker run -itd -p 2000:8000 capstoneimg
 docker tag capstoneimg:latest faizul04/dev/capstoneimg:latest
 docker tag capstoneimg:latest faizul04/prod/capstoneimg:latest
 
